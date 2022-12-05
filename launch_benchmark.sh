3b74f142bc93f91e76cbd9fd4fe66a2377c61f3d
Du, Jun <jun.du@intel.com>
2022-12-05 07:52:21 +0000

    update requirments

--- launch_benchmark.sh
+++ launch_benchmark.sh
@@ -9,6 +9,8 @@ function main {
     set_environment
 
     # requirements
+    pip uninstall detectron2 -y
+    pip install timm==0.6.12
     pip install pycocotools==2.0.5 antlr4-python3-runtime==4.9.3
     python setup.py develop
     pip install omegaconf
